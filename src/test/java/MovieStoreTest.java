29bd836201d2443f02db266dfd1d6a7ef938b609
joaossmacedo <joaossmacedo@gmail.com>
2019-04-03 22:02:06 -0300

    First Test created

--- src/test/java/MovieStoreTest.java
+++ src/test/java/MovieStoreTest.java
@@ -1,7 +1,18 @@
+import model.Movie;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MovieStoreTest {
     @Test
-    public void name() {
+    public void returnsNoResultsWhenNoTitlePartiallyMatchSearch() {
+        MovieStore movieStore = new MovieStore();
+
+        List<Movie> results = movieStore.findByPartialTitle("abc");
+
+        assertThat(results.size(), is(0));
     }
 }
